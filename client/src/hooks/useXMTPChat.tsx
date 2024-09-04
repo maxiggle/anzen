4a94c358193d94dd3c2395134466bf5c1216febb
Treasure O <treasure@tronweb.co>
2024-09-04 18:26:20 +0100

    feat: updated the xmtp

--- client/src/hooks/useXMTPChat.tsx
+++ client/src/hooks/useXMTPChat.tsx
@@ -1,13 +1,22 @@
-import { JsonRpcSigner } from "ethers";
-import { useState } from "react";
+import { JsonRpcSigner, CloudflareProvider } from "ethers";
+import { FormEvent, useState } from "react";
 import usePersistentState from "./usePersistentState";
-import { connect, loadKeys, storeKeys } from "../utils";
+import { connect, isValidEthereumAddress, loadKeys, storeKeys } from "../utils";
 import { Client } from "@xmtp/xmtp-js";
 import config from "../utils/config";
 
 export default function useXMTPChat() {
   const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
   const [client, setClient] = useState<Client | null>(null);
+
+  const [conversationFound, setConversationFound] = useState(false);
+  const [peerAddress, setPeerAddress] = useState<string | null>(null);
+  const [loadingResolve, setLoadingResolve] = useState(false);
+  const [canMessage, setCanMessage] = useState(false);
+  const [createNew, setCreateNew] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [message, setMessage] = useState("");
+
   const [signerAddress, setSignerAddress] = usePersistentState<string | null>(
     "anzen:signer:address",
     null
@@ -51,28 +60,94 @@ export default function useXMTPChat() {
       });
       storeKeys(signerAddress, keys as Buffer);
     }
+
     const xmtp = await Client.create(null, {
       env: config.xmtp.environment,
       privateKeyOverride: keys,
     });
 
-    setNetworkOn(!!xmtp.address);
+    setNetworkOn(Boolean(xmtp.address));
     setClient(xmtp);
     // if (isConsent) {
     //   //Refresh consent
     //   await xmtp.contacts.refreshConsentList();
     // }
   };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSearchChange = async (e: FormEvent | any) => {
+    setCreateNew(false);
+    setConversationFound(false);
+    setSearchTerm(e.target.value);
+    console.log("handleSearchChange", e.target.value);
+    setMessage("Searching...");
+    const addressInput = e.target.value;
+    const isEthDomain = /\.eth$/.test(addressInput);
+    let resolvedAddress = addressInput;
+    if (isEthDomain) {
+      setLoadingResolve(true);
+      try {
+        const provider = new CloudflareProvider();
+        resolvedAddress = await provider.resolveName(resolvedAddress);
+      } catch (error) {
+        console.log(error);
+        // setMessage("Error resolving address");
+        setCreateNew(false);
+      } finally {
+        setLoadingResolve(false);
+      }
+    }
+    console.log("resolvedAddress", resolvedAddress);
+    if (resolvedAddress && isValidEthereumAddress(resolvedAddress)) {
+      processEthereumAddress(resolvedAddress);
+      setSearchTerm(resolvedAddress); // <-- Add this line
+    } else {
+      setMessage("Invalid Ethereum address");
+      setPeerAddress(null);
+      setCreateNew(false);
+      setCanMessage(false);
+    }
+  };
+
+  const processEthereumAddress = async (address: string) => {
+    setPeerAddress(address);
+    if (address === client?.address) {
+      setMessage("No self messaging allowed");
+      setCreateNew(false);
+      // setCanMessage(false);
+    } else {
+      const canMessageStatus = await client?.canMessage(address);
+      if (canMessageStatus) {
+        setPeerAddress(address);
+        // setCanMessage(true);
+        setMessage("Address is on the network ✅");
+        setCreateNew(true);
+      } else {
+        //  setCanMessage(false);
+        setMessage("Address is not on the network ❌");
+        setCreateNew(false);
+      }
+    }
+  };
+
   return {
+    handleSearchChange,
     walletConnected,
     setWalletConnected,
     setSignerAddress,
-    signerAddress,
     initXmtpWithKeys,
     startConnection,
+    signerAddress,
     xmtp: client,
+    loadingResolve,
+    conversationFound,
     setSigner,
+    createNew,
     networkOn,
+    searchTerm,
+    peerAddress,
+    canMessage,
+    message,
     signer,
   };
 }
