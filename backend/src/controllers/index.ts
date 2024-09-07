0b5cff1384cbed4859783366a890daaf3d36e470
Treasure O <treasure@tronweb.co>
2024-09-07 12:41:21 +0100

    fix: event reservation events

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -2,8 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import User from "../database/models/User";
 
 export const Register = async (req: Request, res: Response, next: NextFunction) => {
-  const { email, password, publicKey } = req.body;
-  const user = await User.findOne({ where: { email } });
+  const { firstName, lastName, email, publicKey } = req.body;
+  let user = await User.findOne({ where: { email } });
+
+  if (user) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  user = await User.create({
+    firstName,
+    lastName,
+    email,
+    publicKey,
+  });
+
+  res.json({ user });
+}
+
+export const Login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({ where: { email } });
 }
 
 
